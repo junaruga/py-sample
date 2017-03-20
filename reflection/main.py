5d69edcdf67b610aa3565565aae3ca5a78bf7419
Jun Aruga <jaruga@redhat.com>
2017-03-20 10:54:59 +0100

    Fix reflection sample.

--- reflection/main.py
+++ reflection/main.py
@@ -7,16 +7,28 @@ def import_class(class_name):
     components = class_name.split('.')
     module = __import__(components[0])
     for comp in components[1:]:
+        # print(repr(comp))
         module = getattr(module, comp)
     return module
 
 
 if __name__ == '__main__':
     a = ClassA()
+    print(repr(a))
     a.hello()
 
     b = ClassB()
+    print(repr(b))
     b.hello()
 
-    reflection_a =  import_class(a.filea.ClassA)
+    reflection_clsa = import_class('a.filea.ClassA')
+    print(repr(reflection_clsa))
+    reflection_a = reflection_clsa()
+    print(repr(reflection_a))
     reflection_a.hello()
+
+    reflection_clsb = import_class('a.b.fileb.ClassB')
+    print(repr(reflection_clsb))
+    reflection_b = reflection_clsb()
+    print(repr(reflection_b))
+    reflection_b.hello()
